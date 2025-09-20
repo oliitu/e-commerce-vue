045f0b482eb894f6d839ddcdd11e26979ee43d5e
Adao Iturrusgarai <adaoiturrusgarai@Adaos-MacBook-Air.local>
2025-09-20 17:38:21 -0300

    ss

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,13 +1,46 @@
 <script setup>
-
+import Link from '@/components/Link.vue';
 </script>
 
 <template>
-  <div class="mt-20">
+  <div class="mt-40">
     <Link 
     to="products"
     >Volver
-    </Link>
+    </Link> 
     <h1 class="text-4xl font-black my-10 text-center">Nuevo Producto</h1>
+    <FormKit 
+    type="form" 
+    submit-label="Agregar producto">
+      <FormKit class="bg-white"
+      type="text" 
+      label="Nombre" 
+      name="name" 
+      placeholder="Nombre del producto" 
+      validation="required" 
+      :validation-messages="{required: 'Poné el nombre del producto q es obligatorio'}"/>
+      <FormKit 
+      type="file" 
+      label="Imagen Producto" 
+      name="image" 
+      validation="required" 
+      :validation-messages="{required: 'Poné la imagen del producto q es obligatoria'}"
+      accept=".jpg" 
+      multiple="true"/>
+      <FormKit 
+      type="select" 
+      label="Categoria" 
+      name="category" 
+      validation="required" 
+      :validation-messages="{required: 'Seleccioná la categoria q es obligatoria'}"/>
+      <FormKit 
+      type="numbre" 
+      label="Precio" 
+      name="price" 
+      placeholder="Precio del producto" 
+      validation="required" 
+      :validation-messages="{required: 'Poné el precio q es obligatorio'}"
+      min="1"/>
+    </FormKit>
   </div>
 </template>
--- src/views/admin/ProductsView.vue
+++ src/views/admin/ProductsView.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+import Link from '@/components/Link.vue';
 </script>
 
 <template>
-  <div class="mt-40">
+  <div class="justify-items-center mt-40">
+    <Link class=""
+    to="new-product"
+    >nuevo producto
+    </Link>
     <h1 class="text-4xl font-black my-10 text-center">Soy productos</h1>
   </div>
 </template>
